271de9df69e79b4ea34f8f301f98d3d41b7fd044
ericmaeda <ericmaeda41@gmail.com>
2025-06-25 21:21:20 +0700

    Adding Selection Sort and its main program to execute the algorithm

---
+++ src/SelectionSort/MainProgram.java
@@ -0,0 +1,18 @@
+package SelectionSort;
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        int[] array = {
+                2,8,1,3,5,2,4,7
+        };
+
+        SelectionSort SortArray = new SelectionSort();
+
+        SortArray.Sort(array);
+
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+}
---
+++ src/SelectionSort/SelectionSort.java
@@ -0,0 +1,18 @@
+package SelectionSort;
+
+public class SelectionSort {
+
+    public void Sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+}
