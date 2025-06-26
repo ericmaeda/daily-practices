d6536e92188fa58e1d7fbf4e554da5bdaaac5bfb
ericmaeda <ericmaeda41@gmail.com>
2025-06-26 09:43:21 +0700

    Adding Selection Sort and its main program to execute the algorithm

--- src/InsertionSort/InsertionSort.java
+++ src/InsertionSort/InsertionSort.java
@@ -5,12 +5,12 @@ public class InsertionSort {
     public void Sort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
             int temp = arr[i];
-            int j = i;
-            while ((j > 0) && (temp < arr[j-1])) {
-                arr[j] = arr[j-1];
-                j--;
+            int k = i;
+            while ((k > 0) && (temp < arr[k-1])) {
+                arr[k] = arr[k-1];
+                k--;
             }
-            arr[j] = temp;
+            arr[k] = temp;
         }
     }
 }
--- src/InsertionSort/MainProgram.java
+++ src/InsertionSort/MainProgram.java
@@ -5,12 +5,15 @@ public class MainProgram {
     public static void main(String[] args) {
         int[] array = {
                 2,1,8,4,2,9,1,7
-        };
+        }; // array to be sorted
 
+        // Initiate InsertionSort object
         InsertionSort SortArray = new InsertionSort();
 
+        // Sort the array using Sort() method
         SortArray.Sort(array);
 
+        // Print the array after it's sorted
         for (int k : array) {
             System.out.print(k + " ");
         }
