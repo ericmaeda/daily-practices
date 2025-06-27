d872a5f2d846b1d0e07521ef0148ad317ad6ca7e
ericmaeda <ericmaeda41@gmail.com>
2025-06-27 22:48:19 +0700

    Implementing basic django models, routing, and unit testing

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def show_main(request):
+    context = {
+        'name': 'ericmaeda',
+        'class': 'PBP F'
+    }
+
+    return render(request, "main.html", context)
