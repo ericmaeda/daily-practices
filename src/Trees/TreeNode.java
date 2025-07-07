263c2a70a9c4a3ff225571bc958f7819c5a4759e
ericmaeda <ericmaeda41@gmail.com>
2025-07-07 18:46:04 +0700

    Learned about trees and its modifying methods.

---
+++ src/Trees/TreeNode.java
@@ -0,0 +1,21 @@
+package Trees;
+
+public class TreeNode {
+
+    long value;
+    TreeNode n1;
+    TreeNode n2;
+    TreeNode n3;
+
+    public TreeNode() {
+        this.n1 = null;
+        this.n2 = null;
+        this.n3 = null;
+    }
+
+    public void addNode(int target, TreeNode n) {
+        if (this.value == target) {
+
+        }
+    }
+}
