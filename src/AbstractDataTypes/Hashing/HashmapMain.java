648147ed12bd0910209fd3563f164b4533979381
ericmaeda <ericmaeda41@gmail.com>
2025-07-09 16:19:38 +0700

    Modified something in HashmapMain.java.... idk

--- src/AbstractDataTypes/Hashing/HashmapMain.java
+++ src/AbstractDataTypes/Hashing/HashmapMain.java
@@ -6,14 +6,14 @@ public class HashmapMain {
     public static void main(String[] args) {
         HashMap<String, String> capital = new HashMap<>();
 
-        capital.put("England", "London");
-        capital.put("Portugal", "Lisbon");
+        capital.put("England", "London"); // Putting new <String, String> objects inside the hashmap
+        capital.put("Portugal", "Lisbon"); // with following format : (<key>, <value>)
         capital.put("Argentina", "Buenos Aires");
         capital.put("Spain","Madrid");
         capital.put("Netherlands","Amsterdam");
         capital.put("Germany","Berlin");
 
-        System.out.println(capital.containsValue("Buenos Aires"));
+        System.out.println(capital.containsValue("Buenos Aires")); // check if the HashMap contains "Buenos Aires" as the value
         System.out.println(capital.toString());
         System.out.println(capital.get("Spain"));
 
